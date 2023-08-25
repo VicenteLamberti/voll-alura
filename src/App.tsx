579437df0fcb3a45178151e97bc3104299f66704
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-08-25 09:03:23 -0300

    Criando component Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
+import Container from './components/Container';
 import Header from './components/Header';
 
 function App() {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <Container/>
+    </>
   );
 }
 
