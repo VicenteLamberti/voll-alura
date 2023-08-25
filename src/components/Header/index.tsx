53869867c2bbff977114bd462a822c1dfd0ebbb6
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-08-25 08:57:55 -0300

    Personalizando Header com styled components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,37 @@
+import styled from 'styled-components';
 import logo from './assets/logo.png';
 import perfil from './assets/perfil.png';
 
+const HeaderStyled = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 2em 4em;
+`
+
+const ContainerStyled = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-grow: .1;
+`
+
+const LinkStyled = styled.a`
+    color: var(--azul-escuro);
+    font-weight: 700;
+`
+
 function Header(){
     return (
-        <header>
+        <HeaderStyled>
             <img src={logo} alt="Logo da página"/>
-            <div>
+            <ContainerStyled>
                 <img src={perfil} alt="Foto do perfil do usuário"/>
-                <a href="#">Sair</a>
-            </div>
+                <LinkStyled href="#">Sair</LinkStyled>
+            </ContainerStyled>
 
-        </header>
+        </HeaderStyled>
     )
 }
+
+export default Header;
